59db45ffe1f88b347722ea487caca1a6506407ce
DrSuess2 <truccmom@yahoo.com>
2014-08-15 21:25:46 -0400

    no stall program
    when the motors are stalling, they will shut down so they don't burn
    out.

---
+++ no stall.c
@@ -0,0 +1,21 @@
+
+int MEN;
+task main()
+{
+nMotorEncoder[motorB] = 0;
+
+	while(true)
+	{
+		motor[motorB] = 50;
+		MEN = nMotorEncoder[motorB];
+		if(motor[motorB] != 0)
+		{
+			if(nMotorEncoder[motorB] > MEN - 3 && nMotorEncoder[motorB] < MEN + 10)
+			{
+				motor[motorB] = 0;
+			}
+		}
+		MEN = nMotorEncoder[motorB];
+	}
+
+}
